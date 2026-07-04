93dd258ab3731b4cc907d7e2471357282721ace6
jgabaut <109908086+jgabaut@users.noreply.github.com>
2026-07-04 06:58:45 +0000

    chore: proper usage of new_command for _WIN32

--- src/main.c
+++ src/main.c
@@ -21,7 +21,18 @@ int main(int argc, char** argv) {
     res = run_command_sync(kmd);
     printf("Res: {%s}\n", res ? "ok" : "fail");
 
+#ifndef _WIN32
     Komando foo_kmd = new_command_kls_t(2, args, kls_t);
+#else
+    const char* foo_args[] = {
+        "cmd.exe",
+        "/C",
+        "echo foo",
+        NULL,
+    };
+    Komando foo_kmd = new_command_kls_t(3, foo_args, kls_t);
+#endif // _WIN32
+
     res = run_command_sync(foo_kmd);
     printf("Res: {%s}\n", res ? "ok" : "fail");
 
--- src/main_plain.c
+++ src/main_plain.c
@@ -14,7 +14,18 @@ int main(int argc, char** argv) {
     printf("Res: {%s}\n", res ? "ok" : "fail");
     free_command(&cmd);
 
+#ifndef _WIN32
     Komando foo_cmd = new_command(2, args);
+#else
+    const char* foo_args[] = {
+        "cmd.exe",
+        "/C",
+        "echo foo",
+        NULL,
+    };
+    Komando foo_cmd = new_command(3, foo_args);
+#endif // _WIN32
+
     res = run_command_sync(foo_cmd);
     printf("Res: {%s}\n", res ? "ok" : "fail");
     free_command(&foo_cmd);
